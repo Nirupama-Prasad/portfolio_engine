7bb65a7043f89252a993cd988217a1810a61dde1
nirupamaprasad <nirupama.prasad@gmail.com>
2017-05-05 14:28:56 -0700

    added basic portfolio ds

--- stock_calc.py
+++ stock_calc.py
@@ -1,2 +1,29 @@
 
+from yahoo_finance import Share as sh
 
+
+growth_stocks = ['AAPL', 'FB', 'VMW']
+
+dictionary_strategies = {
+	'growth' : growth_stocks
+	}
+
+
+def get_portfolio(amount, stock_type):
+	stock_dictionary = {}
+
+	for each_stock in stock_type:
+		internal_dictionary = {}
+		stock = sh(each_stock)
+		internal_dictionary['price'] = stock.get_price()
+		internal_dictionary['peg'] = stock.get_price_earnings_growth_ratio()
+		stock_dictionary[each_stock] = internal_dictionary
+
+	print stock_dictionary
+
+
+def test_command_line():
+	get_portfolio(5000, dictionary_strategies['growth'])
+
+
+test_command_line()
--- todo.py
+++ todo.py
@@ -8,6 +8,15 @@ ALGORITHM:
 4. get amount per stock + round down number of stocks
 5. figure out what to do with remainder
 
-FRONT END:
+FRONT END INPUT:
 -----------
 1. dropdown lists for strategy picking
+2. text box for amount
+3. portfolio values for multiple strategy
+
+FRONT END OUTPUT:
+-----------
+1. P elements with portfolio details:
+2. for each in stock:
+	amount spent
+3. portfolio image itself
